2acbfc7315cc73980bd48dbb762405841fd11c4f
jhamPac <cloud@kevia.me>
2021-06-13 13:49:01 -0700

    refactor: separate program from module

--- src/Palindrome.hs
+++ src/Palindrome.hs
@@ -1,3 +1,5 @@
+module Palindrome where
+
 import Data.Char
 
 isPalindrome :: String -> Bool
@@ -10,21 +12,8 @@ isEmptyWord word =
         "" -> Nothing
         _ -> Just (isPalindrome word)
 
-verbose :: String -> String
-verbose word =
-    case (isEmptyWord word) of
-        Nothing -> "Please enter a word"
-        Just False -> "Sorry, this is not a palindrome"
-        Just True -> "Palindrome for sure!"
-
 allLowerCase :: String -> String
 allLowerCase = map toLower
 
-sanitizeForPalinDrome :: String -> String
-sanitizeForPalinDrome = filter (\x -> not (isSpace x)) . allLowerCase
-
-main :: IO ()
-main = 
-    do 
-        word <- getLine
-        print (isPalindrome $ sanitizeForPalinDrome word)
+sanitize :: String -> String
+sanitize = filter (\x -> not (isSpace x)) . allLowerCase
