248ff13fc230d6c4d98c4a502913fa503365a2a6
jhamPac <cloud@kevia.me>
2021-06-13 11:11:07 -0700

    chore: :)

--- src/Palindrome.hs
+++ src/Palindrome.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 isPalindrome :: String -> Bool
 isPalindrome word = 
     word == reverse word
@@ -8,8 +10,26 @@ isEmptyWord word =
         "" -> Nothing
         _ -> Just (isPalindrome word)
 
+verbose :: String -> String
+verbose word =
+    case (isEmptyWord word) of
+        Nothing -> "Please enter a word"
+        Just False -> "Sorry, this is not a palindrome"
+        Just True -> "Palindrome for sure!"
+
+allLowerCase :: String -> String
+allLowerCase = map toLower
+
+-- myMap :: (a -> a) -> [a] -> [a]
+-- myMap fn [] = []
+-- myMap fn (x:xs) = fn x : myMap fn xs
+
+isPalindromeIgnoringCase :: String -> Bool
+isPalindromeIgnoringCase word = 
+    isPalindrome (allLowerCase word)
+
 main :: IO ()
 main = 
     do 
         word <- getLine
-        print (isEmptyWord word)
+        print (isPalindromeIgnoringCase word)
