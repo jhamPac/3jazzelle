7f900db0763aa486cb3bdd0b40a48d302867c7c1
jhamPac <cloud@kevia.me>
2021-06-13 12:05:01 -0700

    refactor: remove unused fns

--- src/Palindrome.hs
+++ src/Palindrome.hs
@@ -20,21 +20,6 @@ verbose word =
 allLowerCase :: String -> String
 allLowerCase = map toLower
 
--- myMap :: (a -> a) -> [a] -> [b]
--- myMap fn [] = []
--- myMap fn (x:xs) = fn x : myMap fn xs
-
-ignoreCase :: String -> Bool
-ignoreCase word = 
-    isPalindrome (allLowerCase word)
-
-removeSpaces :: String -> String
-removeSpaces phrase =
-    case phrase of
-        [] -> []
-        (' ':xs) -> removeSpaces xs
-        (x:xs) -> x : removeSpaces xs
-
 sanitizeForPalinDrome :: String -> String
 sanitizeForPalinDrome = filter (\x -> not (isSpace x)) . allLowerCase
 
