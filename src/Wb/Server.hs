22c9807435ae1875472e7a05c2fb1dc1b288575b
jhamPac <cloud@kevia.me>
2021-06-14 12:04:08 -0700

    feat: simple web server done

--- src/Wb/Server.hs
+++ src/Wb/Server.hs
@@ -14,8 +14,31 @@ import qualified Data.ByteString.Builder        as BSB
 
 -- network
 import Network.Socket (Socket)
-import qualified Network.Socket.ByteString as Socket
+import qualified Network.Socket.ByteString      as Socket
 import qualified Network.Socket.ByteString.Lazy as LSocket
 
 -- network simple
 import qualified Network.Simple.TCP as NS
+
+server :: (Socket -> IO ()) -> IO a
+server f =
+    NS.serve NS.HostAny "9000" $ \(socket, _socketAddress) ->
+        f socket
+
+helloResponse :: BS.ByteString
+helloResponse =
+    asciiLines
+        [ "HTTP/1.1 200 OK"
+        , "Content-Type: text/plain; charset=us-ascii"
+        , "Content-Length: 7"
+        , ""
+        , "Hello!\n"
+        ]
+
+asciiLines :: [String] -> BS.ByteString
+asciiLines xs = 
+    ASCII.pack (List.intercalate "\r\n" xs)
+
+sayHello :: Socket -> IO ()
+sayHello socket =
+    Socket.sendAll socket helloResponse
